6679ce10f7bac8da707fc8ec31127c19dd91e13b
“Saki <sakisandra.c@gmail.com>
2023-08-03 00:01:51 -0600

    Add api key

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -4,7 +4,7 @@ const getKanji = async () => {
   const res = await fetch(`https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/`, {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_API_KEY!,
+      'X-RapidAPI-Key': 'e87105ca8fmshc0ad9b60476ff6ep1f84adjsn4b0e9e33ff3b',
       'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
     }
   });
@@ -22,7 +22,7 @@ const getSingleKanji = async (type: string, char: string) => {
   const res = await fetch(`https://kanjialive-api.p.rapidapi.com/api/public/${type}/${char}`, {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_API_KEY!,
+      'X-RapidAPI-Key': 'e87105ca8fmshc0ad9b60476ff6ep1f84adjsn4b0e9e33ff3b',
       'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
     }
   })
