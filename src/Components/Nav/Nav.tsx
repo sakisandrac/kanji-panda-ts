88dee978fe2e7ba73068930b3f5fa0127d2ca585
Sandra Chatphatthanasiri <sakisandra.c@gmail.com>
2023-11-22 11:22:19 -0700

    Add loading state

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -25,16 +25,16 @@ const Nav: React.FC<NavProps> = ({logIn, user}) => {
         <Link to='/' aria-label="link to homepage"><img className='logo-small' src={logoSmall} alt='kanji panda logo small'/></Link>
         <Link to='/' aria-label="link to homepage"><p className='logo-text' aria-label="link to homepage">Kanji Panda</p></Link>
       </div>
-      <div className="nav-link-container">
-        <NavLink className="nav-link text" to="/" aria-label="link to homepage">Home</NavLink>
+      {user && <div className="nav-link-container">
+      <NavLink className="nav-link text" to="/" aria-label="link to homepage">Home</NavLink>
         <NavLink className="nav-link text" to="/saved" aria-label="link to saved kanji page">My Learning</NavLink>
         <NavLink className="nav-link text" to="/search" aria-label="link to search page">Search</NavLink>
         <p className="nav-link text" onClick={logout}>Logout</p>
         <Link className="nav-link icon" to="/" aria-label="link to homepage"><img className='nav-icon' src={home} alt='home icon'/></Link>
         <Link className="nav-link icon" to="/saved" aria-label="link to saved kanji page"><img className='nav-icon' src={saved} alt='saved page icon'/></Link>
         <Link className="nav-link icon" to="/search" aria-label="link to search page"><img className='nav-icon' src={search} alt='search icon'/></Link>
-        {user && <img className='nav-icon' onClick={logout} src={logoutIcon} alt="logout icon"></img>}
-      </div>
+        <img className='nav-icon' onClick={logout} src={logoutIcon} alt="logout icon"></img>
+      </div>}
     </nav>
   )
 }
